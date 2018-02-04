3f93a45bdf0de5570b143bc6fa3a2f432b93accb
Nick Konstantinidis <nick.kon@usask.ca>
2018-02-03 19:27:14 -0600

    Revert "Merge branch 'revert-008fd21e' into 'develop'"
    This reverts merge request !10

---
+++ test/e2e/specs/testserver.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Demo test Google' : function (browser) {
+    console.log('test');
+    browser
+      .url(browser.globals.devServerURL)
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('.btn', 'Toggle alert')
+      .click('.btn')
+      .waitForElementPresent('.alert-info',1000)
+      .click('.btn')
+      .waitForElementNotPresent('.alert-info',1000)
+      .end();
+  }
+};
