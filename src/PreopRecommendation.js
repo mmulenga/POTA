38d39b90b6eb335d52d73cd08fbb447d70383705
Erik <erikschick777@gmail.com>
2018-02-12 16:55:43 -0600

    Implement main algorithm base

--- src/PreopRecommendation.js
+++ src/PreopRecommendation.js
@@ -1,14 +1,19 @@
 // Singleton data, not a class
 import assert from '@/main.js';
-import Tag from '@/tags.js';
+import { Tag, GetExams } from '@/tag.js';
+import { Condition, Exam } from '@/constants.js';
 
-test_collection = {};
+/** Tag definitions **/
+Tag(Condition.HeartFail, [Exam.ECG, Exam.CBC, Exam.RenPanel]);
 
-function addToCollection(t) {
-    assert(t instanceof Tag);
-    test_collection[t.name] = tests;
-};
 
-function getAllTests() {
-    return test_collection;
+function PatientExamsNeeded(patientConditions) {
+    testAggregation = new Set();
+    for (var i = 0; i < patientConditions.length; i++) {
+        exams = GetExams(patientConditions[i])
+        for (var j = 0; j < exams.length; j++) {
+            testAggregation.add(exams[j]);
+        }
+    }
+    return testAggregation
 }
--- src/tag.js
+++ src/tag.js
@@ -1,7 +1,15 @@
-class Tag {
-    constructor(name, tests) {
-        this.name = name;
-        this.tests = tests;
-    }
+test_collection = {};
+
+function Tag(name, tests) {
+    test_collection[name] = tests;
+};
+
+function GetExams(name) {
+    return test_collection[t.name]
 }
 
+function GetAllExams() {
+    return test_collection;
+}
+
+export default { Tag, GetExams, GetAllExams };
