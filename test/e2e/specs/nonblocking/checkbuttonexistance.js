6270245bb117a05a773293022b9a97a963dc3040
nic014 <nic014@mail.usask.ca>
2018-02-14 17:20:10 -0600

    implemented non-blocking tests.

---
+++ test/e2e/specs/nonblocking/checkbuttonexistance.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'Demo test Google' : function (browser) {
+    console.log('test');
+    browser
+      .url(browser.globals.devServerURL)
+      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('#menu_btn', 1000)
+      .waitForElementPresent('#testing_btn', 500)
+      .waitForElementPresent('#help_btn', 1000)
+      .waitForElementPresent('#glossary_btn', 500)
+      .waitForElementPresent('#reset_btn', 500)
+      .waitForElementPresent('#lowrist_btn, 500)
+      .waitForElementPresent('#medrisk_btn', 500)
+      .waitForElementPresent('#highrisk_btn', 500)
+      .end();
+  }
+};
