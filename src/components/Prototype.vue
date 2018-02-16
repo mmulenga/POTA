db2e3ef1eb3e0870502cbd31541e19b04a79709e
Sam Wilkinson <scw528@mail.usask.ca>
2018-02-15 23:08:28 -0600

    skeleton of left menu bar

--- src/components/Prototype.vue
+++ src/components/Prototype.vue
@@ -3,7 +3,30 @@
     <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
     <div class="row">
         <!-- This is the left side view -->
-        <div class="col-6 col-md-4">To Do the left side UI</div>
+        <div class="col-6 col-md-4">
+            <!-- TODO: actually make this do something - when menu button is clicked it should
+            show/hide the options of home/glossary/help. If home is clicked, it should take you
+            back to the main question page, if glossary is clicked it should take to you a page
+            all terminology, if help is clicked, it will direct you to a page for help -->
+            <h4 class="alert-heading"></h4>
+                <button class="btn btn-primary" type="button" id="menu_btn">Menu</button>
+                <hr>
+                <div class="nav flex-column nav-pills py-5" id="v-pills-tab"
+                role="tablist" aria-orientation="vertical">
+                    <a class="nav-link active" id="testing_btn"
+                        data-toggle="pill" href="#v-pills-home"
+                        role="tab" aria-controls="v-pills-home"
+                        aria-selected="true">Testing</a>
+                    <a class="nav-link" id="help_btn"
+                        data-toggle="pill" href="#v-pills-profile"
+                        role="tab" aria-controls="v-pills-profile"
+                        aria-selected="false">Help</a>
+                    <a class="nav-link" id="glossary_btn"
+                        data-toggle="pill" href="#v-pills-settings"
+                        role="tab" aria-controls="v-pills-settings"
+                        aria-selected="false">Glossary</a>
+                </div>
+        </div>
         <!-- This is the mid side view -->
         <div class="col-6 col-md-4">To Do the Mid side UI</div>
         <!-- This is the right side view (UI) -->
@@ -37,7 +60,7 @@
                 </red-text>
                 <br/>
                 <hr>
-                <button class="btn btn-primary" type="button">Restore</button>
+                <button class="btn btn-primary" type="button" id="reset_btn">Restore</button>
             </div>
         </div>
     </div>
