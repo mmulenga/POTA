fa41d0553ce230aff470eabd92e0d2d23545f47e
Jonathan Mak <jom262@mail.usask.ca>
2018-02-18 12:23:19 -0600

    Added snapshot testing to Prototype.vue

---
+++ test/unit/specs/App.test.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import App from '../../../src/components/Prototype';
+
+describe('App.test.js', () => {
+  let cmp;
+  let vm;
+  beforeEach(() => {
+    cmp = Vue.extend(App); // Create a copy of the original component
+    vm = new cmp({
+      data: { // Replace data value with this fake data
+        framework_name: 'Vue',
+      },
+    }).$mount(); // Instances and mounts the component
+  });
+  it('equals framework to VueJS', () => {
+    expect(vm.framework_name).toEqual('Vue');
+  });
+
+  it('has the expected html structure', () => {
+    expect(vm.$el).toMatchSnapshot();
+  });
+});
