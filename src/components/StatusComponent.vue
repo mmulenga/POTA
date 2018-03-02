b634b026d4d593c404790ff64b397dd3e7735848
mjg539 <mjg539@mail.usask.ca>
2018-03-02 14:00:55 -0600

    Rewrote Status

--- src/components/StatusComponent.vue
+++ src/components/StatusComponent.vue
@@ -1,42 +1,30 @@
-<template id="StatusComponent">
-    <div class="group realtimedb">
-        <h1>Co-morbidities</h1>
-        <div class="list">
-            <ul id="conditions-received"
-                v-for="item in conditionsReceived"
-                :key="item.conditionsReceived">
-                    <li> {{item.comorbidity}} </li>
-            </ul>
-        </div>
+<template>
+    <div id="status">
+    <h1>Patient Status</h1>
+    <ol class="list">
+        <status-item
+        v-for="item in conditionsReceived"
+        v-bind:status="item"
+        v-bind:key="item.id">
+        </status-item>
+    </ol>
     </div>
 </template>
 
 <script>
-// import Vue from 'vue';
+import Vue from 'vue';
 import { Condition } from '@/constants';
 
-/*
-const status = new Vue({
-  el: '#status',
-  data: {
-    conditionsReceived: [
-      { comorbidity: Condition.CAD },
-    ],
-  },
+Vue.component('status', {
+  props: ['status'],
+  template: '<li>{{ status.item }}</li>',
+  conditionsReceived: [
+    { id: 0, comorbidity: Condition.CAD },
+  ],
 });
-
-status.conditionsReceived.push({ comorbidity: Condition.AtrialFib });
-*/
 
 export default {
   name: 'StatusComponent',
-  data() {
-    return {
-      conditionsReceived: [
-        { comorbidity: Condition.CAD },
-      ],
-    };
-  },
 };
 
 </script>
