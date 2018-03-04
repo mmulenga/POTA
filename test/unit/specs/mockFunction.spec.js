7f4e7683b7c2549004b558e92944c3607030b2db
Dale Fu <dale.fu@usask.ca>
2018-03-04 16:41:17 -0600

    Completed and updated the mock functions for the backend algorithm.

--- test/unit/specs/mockFunction.spec.js
+++ test/unit/specs/mockFunction.spec.js
@@ -1,4 +1,6 @@
-import { Exam } from '@/constants';
+import { Exam, Condition } from '@/constants';
+import { ExamSummary } from '@/PreopRecommendation';
+import { ConditionalExam, GetConditionalExams } from '@/tag';
 
 function forEach(items, callback) {
   for (let index = 0; index < items.length; index += 1) {
@@ -20,6 +22,7 @@ describe('Sample testing mocking framework in Jest', () => {
 
 const mockGetExams = jest
   .fn()
+  // Last test
   .mockReturnValue([])
   // Test 1
   .mockReturnValueOnce(['G&S', 'ECG', 'CBC', 'Renal Panel (Creat + Lytes)', 'PTT/INR', 'LFT', 'Gluc', 'HbA1C', 'CXR'])
@@ -42,78 +45,106 @@ const mockGetExams = jest
   .mockReturnValueOnce(['ECG'])
   .mockReturnValueOnce(['CBC', 'ECG', 'G&S']);
 
+const mockGetConditionalExams = jest
+  .fn()
+  // Last test
+  .mockReturnValue([])
+  // Test 1
+  .mockReturnValueOnce(GetConditionalExams(Condition.VHD))
+  // Test 2
+  .mockReturnValueOnce(GetConditionalExams(Condition.Diabetes))
+  .mockReturnValueOnce(GetConditionalExams(Condition.Malignancy))
+  // Test 3
+  .mockReturnValueOnce(GetConditionalExams(Condition.VHD))
+  .mockReturnValueOnce(GetConditionalExams(Condition.Malignancy))
+  .mockReturnValueOnce(GetConditionalExams(Condition.Diabetes))
+  // Test 4
+  .mockReturnValueOnce(GetConditionalExams(Condition.Malignancy))
+  .mockReturnValueOnce(GetConditionalExams(Condition.Diabetes))
+  .mockReturnValueOnce(GetConditionalExams(Condition.VHD))
+  .mockReturnValueOnce(GetConditionalExams(Condition.AntiCoagulants))
+  // Test 5
+  .mockReturnValueOnce(GetConditionalExams(Condition.Antiplatelet))
+  .mockReturnValueOnce(GetConditionalExams(Condition.VHD))
+  .mockReturnValueOnce(GetConditionalExams(Condition.Diabetes))
+  .mockReturnValueOnce(GetConditionalExams(Condition.Malignancy))
+  .mockReturnValueOnce(GetConditionalExams(Condition.AntiCoagulants));
+
+
 function PatientExamsNeeded(patientConditions) {
-  const testAggregation = new Set();
+  const examAggregation = new Set();
+  const conditionalExamAggregation = [];
   for (let i = 0; i < patientConditions.length; i += 1) {
     const exams = mockGetExams();
     for (let j = 0; j < exams.length; j += 1) {
-      testAggregation.add(exams[j]);
+      examAggregation.add(exams[j]);
     }
   }
-  return testAggregation;
+  for (let i = 0; i < patientConditions.length; i += 1) {
+    const conditionalExams = mockGetConditionalExams();
+    for (let j = 0; j < conditionalExams.length; j += 1) {
+      // Make a new copy of the ConditionalExam with any preconsidered exams filtered out
+      const ce = new ConditionalExam(
+        conditionalExams[j].conditionPhrase,
+        conditionalExams[j].exams.filter(e => !(examAggregation.has(e))),
+      );
+      // Only aggregate this conditionalExam if it is not entirely preconsidered
+      if (ce.exams.length > 0) {
+        conditionalExamAggregation.push(ce);
+      }
+    }
+  }
+  return new ExamSummary([...examAggregation], conditionalExamAggregation);
 }
 
-describe('Testing mock function for GetExams() for PatientExamsNeeded()', () => {
-  it('returns correct set of list of exams for test 1 of mockGetExams()', () => {
-    const requiredExams = new Set();
-    requiredExams.add(Exam.GnS);
-    requiredExams.add(Exam.ECG);
-    requiredExams.add(Exam.CBC);
-    requiredExams.add(Exam.RenPanel);
-    requiredExams.add(Exam.PTTINR);
-    requiredExams.add(Exam.LFT);
-    requiredExams.add(Exam.Gluc);
-    requiredExams.add(Exam.HbA1C);
-    requiredExams.add(Exam.CXR);
-    expect(PatientExamsNeeded([''])).toEqual(requiredExams);
+let result;
+
+describe('Testing mock function for GetExams() and GetConditionalExams() for PatientExamsNeeded()', () => {
+  it('returns correct array of list of exams for test 1 of mockGetExams() and mockGetConditionalExams()', () => {
+    const requiredExams = [Exam.GnS, Exam.ECG, Exam.CBC, Exam.RenPanel, Exam.PTTINR,
+      Exam.LFT, Exam.Gluc, Exam.HbA1C, Exam.CXR];
+    result = PatientExamsNeeded(['']);
+    expect(result.exams).toEqual(requiredExams);
+    expect(result.conditionalExams).toEqual([]);
   });
 
-  it('returns correct set of list of exams for test 2 of mockGetExams()', () => {
-    const requiredExams = new Set();
-    requiredExams.add(Exam.GnS);
-    requiredExams.add(Exam.CBC);
-    requiredExams.add(Exam.PTTINR);
-    requiredExams.add(Exam.Gluc);
-    requiredExams.add(Exam.CXR);
-    expect(PatientExamsNeeded(['', ''])).toEqual(requiredExams);
+  it('returns correct array of list of exams for test 2 of mockGetExams() and mockGetConditionalExams()', () => {
+    const requiredExams = [Exam.GnS, Exam.CBC, Exam.PTTINR, Exam.Gluc, Exam.CXR];
+    result = PatientExamsNeeded(['', '']);
+    expect(result.exams).toEqual(requiredExams);
+    expect(result.conditionalExams[0].exams).toEqual([Exam.HbA1C]);
+    expect(result.conditionalExams[1].exams).toEqual([Exam.ECG, Exam.RenPanel]);
   });
 
-  it('returns correct set of list of exams for test 3 of mockGetExams()', () => {
-    const requiredExams = new Set();
-    requiredExams.add(Exam.GnS);
-    requiredExams.add(Exam.RenPanel);
-    requiredExams.add(Exam.LFT);
-    requiredExams.add(Exam.HbA1C);
-    requiredExams.add(Exam.CXR);
-    expect(PatientExamsNeeded(['', '', ''])).toEqual(requiredExams);
+  it('returns correct array of list of exams for test 3 of mockGetExams() and mockGetConditionalExams()', () => {
+    const requiredExams = [Exam.RenPanel, Exam.LFT, Exam.HbA1C, Exam.GnS, Exam.CXR];
+    result = PatientExamsNeeded(['', '', '']);
+    expect(result.exams).toEqual(requiredExams);
+    expect(result.conditionalExams[0].exams).toEqual([Exam.CBC]);
+    expect(result.conditionalExams[1].exams).toEqual([Exam.ECG, Exam.PTTINR]);
   });
 
-  it('returns correct set of list of exams for test 4 of mockGetExams()', () => {
-    const requiredExams = new Set();
-    requiredExams.add(Exam.ECG);
-    requiredExams.add(Exam.CBC);
-    requiredExams.add(Exam.RenPanel);
-    requiredExams.add(Exam.PTTINR);
-    requiredExams.add(Exam.LFT);
-    requiredExams.add(Exam.Gluc);
-    requiredExams.add(Exam.HbA1C);
-    requiredExams.add(Exam.CXR);
-    expect(PatientExamsNeeded(['', '', '', ''])).toEqual(requiredExams);
+  it('returns correct array of list of exams for test 4 of mockGetExams() and mockGetConditionalExams()', () => {
+    const requiredExams = [Exam.CBC, Exam.Gluc, Exam.HbA1C, Exam.CXR, Exam.ECG,
+      Exam.LFT, Exam.PTTINR, Exam.RenPanel];
+    result = PatientExamsNeeded(['', '', '', '']);
+    expect(result.exams).toEqual(requiredExams);
+    expect(result.conditionalExams).toEqual([]);
   });
 
-  it('returns correct set of list of exams for test 5 of mockGetExams()', () => {
-    const requiredExams = new Set();
-    requiredExams.add(Exam.GnS);
-    requiredExams.add(Exam.ECG);
-    requiredExams.add(Exam.CBC);
-    requiredExams.add(Exam.Gluc);
-    requiredExams.add(Exam.CXR);
-    expect(PatientExamsNeeded(['', '', '', '', ''])).toEqual(requiredExams);
+  it('returns correct array of list of exams for test 5 of mockGetExams() and mockGetConditionalExams()', () => {
+    const requiredExams = [Exam.CBC, Exam.Gluc, Exam.ECG, Exam.CXR, Exam.GnS];
+    result = PatientExamsNeeded(['', '', '', '', '']);
+    expect(result.exams).toEqual(requiredExams);
+    expect(result.conditionalExams[0].exams).toEqual([Exam.PTTINR]);
+    expect(result.conditionalExams[1].exams).toEqual([Exam.HbA1C]);
+    expect(result.conditionalExams[2].exams).toEqual([Exam.RenPanel]);
   });
 
-  it('returns correct set of list of exams for test 6 of mockGetExams()', () => {
-    const requiredExams = new Set();
-    expect(PatientExamsNeeded([''])).toEqual(requiredExams);
+  it('returns correct array of list of exams for test 6 of mockGetExams() and mockGetConditionalExams()', () => {
+    const requiredExams = [];
+    result = PatientExamsNeeded('');
+    expect(result.exams).toEqual(requiredExams);
+    expect(result.conditionalExams).toEqual([]);
   });
 });
-
