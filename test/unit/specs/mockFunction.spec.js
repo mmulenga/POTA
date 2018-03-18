4f26e48a8aac3c92c5200f356075256e103a00cb
Dale Fu <dale.fu@usask.ca>
2018-03-18 16:38:46 -0600

    Removed temp mock test

--- test/unit/specs/mockFunction.spec.js
+++ test/unit/specs/mockFunction.spec.js
@@ -2,24 +2,6 @@ import { Exam, Condition } from '@/constants';
 import { ExamSummary } from '@/PreopRecommendation';
 import { ConditionalExam, GetConditionalExams } from '@/tag';
 
-function forEach(items, callback) {
-  for (let index = 0; index < items.length; index += 1) {
-    callback(items[index]);
-  }
-}
-const mockCallback = jest.fn();
-forEach([0, 1], mockCallback);
-
-
-describe('Sample testing mocking framework in Jest', () => {
-  it('returns correct callback function tests', () => {
-    expect(mockCallback.mock.calls.length).toBe(2);
-    expect(mockCallback.mock.calls[0][0]).toBe(0);
-    expect(mockCallback.mock.calls[1][0]).toBe(1);
-  });
-});
-
-
 const mockGetExams = jest
   .fn()
   // Last test
