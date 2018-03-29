af54024604c84189950694a35ce971009baa2039
Dale Fu <dale.fu@usask.ca>
2018-03-29 17:32:15 -0600

    Added tests for ExamValidity()

--- test/unit/specs/validity.spec.js
+++ test/unit/specs/validity.spec.js
@@ -1,6 +1,6 @@
-import { Condition, Exam } from '@/constants';
-import { PatientExamsNeeded, ExamValidity } from '@/PreopRecommendation';
-import { GetExams, GetAllExams, GetConditionalExams, Tag, GetExamValidity, Validity } from '@/tag';
+import { Exam } from '@/constants';
+import { ExamValidity } from '@/PreopRecommendation';
+import { GetExamValidity } from '@/tag';
 
 let result;
 
@@ -86,3 +86,84 @@ describe('Testing GetExamValidity() with all preop examinations', () => {
   });
 });
 
+describe('Testing ExamValidity() with all preop examinations', () => {
+  it('returns the correct validity period exams for GnS', () => {
+    result = ExamValidity(Exam.GnS);
+    expect(result).toEqual(expect.stringContaining('1 month'));
+  });
+  it('correct validity period for GnS to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for ECG', () => {
+    result = ExamValidity(Exam.ECG);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for ECG to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for CBC', () => {
+    result = ExamValidity(Exam.CBC);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for CBC to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for RenPanel', () => {
+    result = ExamValidity(Exam.RenPanel);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for RenPanel to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for PTTINR', () => {
+    result = ExamValidity(Exam.PTTINR);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for PTTINR to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for LFT', () => {
+    result = ExamValidity(Exam.LFT);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for LFT to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for Gluc', () => {
+    result = ExamValidity(Exam.Gluc);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for Gluc to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for HbA1C', () => {
+    result = ExamValidity(Exam.HbA1C);
+    expect(result).toEqual(expect.stringContaining('3 months'));
+  });
+  it('correct validity period for HbA1C to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for CXR', () => {
+    result = ExamValidity(Exam.CXR);
+    expect(result).toEqual(expect.stringContaining('6 months'));
+  });
+  it('correct validity period for CXR to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns the correct conditional exams for TSH', () => {
+    result = ExamValidity(Exam.TSH);
+    expect(result).toEqual(expect.stringContaining('6 months'));
+  });
+  it('correct validity period for TSH to match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+});
