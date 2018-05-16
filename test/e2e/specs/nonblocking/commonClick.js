eaa8dc090ac845ca0535140fe673f2007e176a9a
Nick Konstantinidis <nick.kon@usask.ca>
2018-05-15 18:42:59 -0600

    added rephrasing of heart failure in check

--- test/e2e/specs/nonblocking/commonClick.js
+++ test/e2e/specs/nonblocking/commonClick.js
@@ -164,8 +164,7 @@ module.exports = {
       .assert.visible('#cv_checkbox_HeartFail:checked',
         'Testing if element <#cv_checkbox_HeartFail> is checked')
       // Checking if Patient Status are updated
-      .assert.containsText('div.list', 'Valvular heart disease')
-      .assert.containsText('div.list', 'Heart failure')  
+      .assert.containsText('div.list', 'Peripheral Vascular disease (PVD) Valvular heart disease / Valve Replacement')  
       // Clicking submit button and checking results 
       .click('#submit_button')
       .assert.visible('#modal_okay')
